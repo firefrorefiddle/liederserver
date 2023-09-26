ad8a72e320283ab823045b9bb9841d3c9983aabd
Mike Hartl <mikehartl17@gmail.com>
2023-09-26 08:31:13 +0200

    HX handlers

--- server.pl
+++ server.pl
@@ -1,36 +1,61 @@
 :- use_module(library(http/http_server)).
 :- use_module(library(http/http_files)).
+:- use_module(library(http/html_write)).
 :- use_module(library(arouter)).
 :- use_module(library(base64)).
 :- use_module(library(lists)).
 :- use_module(db).
 :- use_module(library(persistency)).
 
-:- route_get(/, get_songs).
-:- route_get(song_from_db, get_song).
-:- route_get(song_preview, get_song_preview).
+handle_hx(Handler,_) :- % second param used internally in arouter, is unbound
+    http_current_request(R),
+    (
+	member(hx_target(_), R)
+    ->  Method=reply_htmx
+    ;
+    (   member(hx_boosted(true), R)
+    ->  Method=reply_html
+    ;   Method=reply_standard_layout
+    )
+    ),
+    handle_hx_request(Method, Handler).
+
+hx_route_get(Address, Handler, Options) :-
+    route_get(Address, handle_hx(Handler), Options).
+
+hx_route_get(Address, Handler) :-
+    hx_route_get(Address, Handler, []).
+
+handle_hx_request(Method, Handler) :-
+    call(Handler, Method).
+
+:- hx_route_get(/, get_songs).
+:- hx_route_get(song_from_db, get_song).
+:- hx_route_get(song_preview, get_song_preview).
+:- hx_route_get(edit_song, get_song_editor).
 
 :- http_handler(root(assets/'style.css'), http_reply_file('assets/style.css', []), [id(style_GET)]).
 
 run :- song_db_attach,
        http_server(handle_request, [port(3333)]).
 
-get_songs :-
+get_songs(Respond) :-
     findall(Song-Author-Id-Headers, song_from_db(Id, Song, Author, Headers), Songs),
     sort(Songs, Sorted),
-    reply_standard_layout("Songs",
-			  div([
-				     input([type(text),placeholder("Filter"),
-					   '_'("
-on keyup
+    call(Respond,
+	 "Songs",
+	 div([
+		    input([type(text),placeholder("Filter"),
+			   '_'("
+on keyup		   
    if the event's key is 'Escape'
       set my value to ''
       trigger keyup
    else
       show <li/> in #songlist when its textContent.toLowerCase() contains my value.toLowerCase()")]),
- 
-				     ul([id(songlist)], \song_list(Sorted))
-				 ])).
+		    
+		    ul([id(songlist)], \song_list(Sorted))
+		])).
 
 song_list([]) --> [].
 song_list([Title-Author-Id-Headers|Rest]) -->
@@ -44,38 +69,72 @@ title_attrs(Headers, TitleText) :-
 	    Headers,
 	    TitleText).   
 
-get_song :-
+get_song(Respond) :-
     http_current_request(R),
     http_parameters(R, [id(IdA, [])]),
     atom_number(IdA, Id),
-    song_from_db(Id, Title, _, _),
-    once(song_version(Id, VersionId, _User, Text)),
-    reply_standard_layout(Title,
-			  [ div([class(flex_container)],
-				[ div([class(flex_child)],
-				      pre([], [Text])),
-				  div([class(flex_child), id(preview),
-				       'hx-get'('/song_preview?id='+Id+'&versionId='+VersionId),
-				       'hx-trigger'('load from:body')]				     
-				      ,[])
-				])
-			  ]).
+    get_song(Respond, Id).
 
-get_song_preview :-
+get_song(Respond, Id) :-
+    song_from_db(Id, Title, _, _),    
+    once(song_version(Id, VersionId, _User, Text)),
+    call(Respond,
+	 Title,
+	 [ div([class(flex_container)],
+	       [ div([class(flex_child)],
+		     \disp_lyrics(Id, VersionId, Text)),
+		 div([class(flex_child), id(preview),
+		      'hx-get'('/song_preview?id='+Id+'&versionId='+VersionId),
+		      'hx-trigger'('load from:body')]				     
+		     ,[])
+	       ])
+	 ]).
+
+disp_lyrics(_Id, VersionId, Text) -->    
+    html(div([id("disp_edit")],
+	     [button([value="Edit",
+		      'hx-get'='/edit_song?versionId='+VersionId,
+		      'hx-target'='#disp_edit',
+		      'hx-swap'='outerHtml'],["Edit"]),		    
+	      pre([], [Text])])).
+
+get_song_preview(Respond) :-
     http_current_request(R),    
     http_parameters(R, [versionId(VersionIdA, [])]),
     atom_number(VersionIdA, VersionId),    
-    get_song_preview(VersionId).
+    get_song_preview(Respond, VersionId).
 
-get_song_preview(VersionId) :-
+get_song_preview(Respond, VersionId) :-
     song_version(_, VersionId, _, Text),
     tmp_file(preview, Path),
     open(Path, write, Stream),
     write(Stream, Text),
     close(Stream),
     latex_song(Path, Pngs),
-    reply_htmx(div(\disp_pngs(Pngs))).
-    
+    call(Respond, div(\disp_pngs(Pngs))).
+
+get_song_editor(Respond) :-
+    http_current_request(R),    
+    http_parameters(R, [versionId(VersionIdA, [])]),
+    atom_number(VersionIdA, VersionId),    
+    get_song_editor(Respond, VersionId).
+
+get_song_editor(Respond, VersionId) :-
+    once(song_version(_, VersionId, _, Text)),    
+    call(Respond,
+	 div([id="disp_edit"],
+	     [button([value="Edit",
+		      'hx-get'='/get_song_lyrics?versionId='+VersionId,
+		      'hx-target'='#disp_edit',
+		      'hx-swap'='outerHtml'],["Cancel"]),
+	      input([type=textarea,
+		     style="height:50vh;width:50vw",
+		     rows=100,
+		     cols=80,
+		     id="editor",
+		     value=Text],[])])).
+
+
 disp_pngs([]) --> [].
 disp_pngs([Png|Pngs]) --> disp_png(Png), disp_pngs(Pngs).
 
@@ -87,34 +146,59 @@ disp_png(Png) -->
 	string_concat("data:image/png;base64, ", Base64, Src)
     },
     html(img([style('width:450'),src(Src),alt("Preview")])).
-    
+
 song_title(Song, Headers, Title) :-
     once(member(title-Title, Headers) ; Title = Song).
 
 reply_htmx(Elem) :-
+    retractall(last_request(_)),
+    http_current_request(R),
+    assertz(last_request(R)),
+    
     html_write:html_current_option(content_type(Type)),
     phrase(html(Elem), HTML),
     format('Content-type: ~w~n~n', [Type]),
     html_write:print_html(HTML).
 
+reply_html(Content) :- reply_html('No Title', Content).
+reply_html(Title, Content) :-
+    Header = [title(Title)],
+    reply_standard_layout(Header, Title, Content).
+
+standard_head(Title, Head) :-
+    Head = [title(Title),
+	    script([src="https://code.jquery.com/jquery-3.7.1.slim.min.js", integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=", crossorigin="anonymous"],[]),
+	    script([src("https://unpkg.com/htmx.org@1.9.2"),
+		    integrity("sha384-L6OqL9pRWyyFU3+/bjdSri+iIphTN/bvYyM37tICVyOJkWZLpP2vGn6VUEXgzg6h"),
+		    crossorigin("anonymous")],[]),
+	    script([src("https://unpkg.com/htmx.org/dist/ext/sse.js")],[]),
+	    script([src("https://unpkg.com/hyperscript.org@0.9.11")],[]),
+	    link([rel(stylesheet),
+		  href("https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css"),
+		  integrity("sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu"),
+		  crossorigin("anonymous")],[]),
+	    link([rel("stylesheet"), href("https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap-theme.min.css"), integrity("sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ"), crossorigin("anonymous")],[]),
+	    script([src("https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js"), integrity("sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd"), crossorigin("anonymous")],[]),
+	    link([rel(stylesheet),href('/assets/style.css')])].
+
+:- dynamic last_request/1.
+
+reply_standard_layout(Body) :-
+    reply_standard_layout('No title', Body).
+
 reply_standard_layout(Title, Body) :-
-    reply_html_page([title(Title),
-		     script([src("https://unpkg.com/htmx.org@1.9.2"),
-			     integrity("sha384-L6OqL9pRWyyFU3+/bjdSri+iIphTN/bvYyM37tICVyOJkWZLpP2vGn6VUEXgzg6h"),
-			     crossorigin("anonymous")],[]),
-		     script([src("https://unpkg.com/htmx.org/dist/ext/sse.js")],[]),
-		     script([src("https://unpkg.com/hyperscript.org@0.9.11")],[]),
-		     link([rel(stylesheet),
-			   href("https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css"),
-			   integrity("sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu"),
-			   crossorigin("anonymous")],[]),
-			  link([rel("stylesheet"), href("https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap-theme.min.css"), integrity("sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ"), crossorigin("anonymous")],[]),
-			  script([src("https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js"), integrity("sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd"), crossorigin("anonymous")],[]),
-			  link([rel(stylesheet),href('/assets/style.css')])],
-		     div([id(body),'hx-boost'(true)],
-			 [\navbar,
-			  h1([class(page_title)], Title),
-			  div([],Body)])).
+    standard_head(Title, Head),
+    reply_standard_layout(Head, Title, Body).
+
+reply_standard_layout(Head, Title, Body) :-
+    retractall(last_request(_)),
+    http_current_request(R),
+    assertz(last_request(R)),
+    reply_html_page(Head,
+		    div([id(body),'hx-boost'(true)],
+			[\navbar,
+			 h1([class(page_title)], Title),
+			 div([],Body)])).
 
 navbar -->  html(div([id(navbar),'hx-boost'(true)],
 		     [
@@ -178,11 +262,11 @@ song_header(Stream, Key-Value) :-
 
 song_headers(Stream, Headers) :-
     findall(Key-Value, (song_header(Stream, Key-Value), nonvar(Key)), Headers).
-       
+
 handle_request(Request):-
-(   route(Request)
-->  true
-;   http_dispatch(Request)).
+    (   route(Request)
+    ->  true
+    ;   http_dispatch(Request)).
 
 
 file_lines(Path, Lines) :-
@@ -253,7 +337,7 @@ find_png_files(Basename, Files) :-
       maplist({Basename}/[I,File]>>atomic_list_concat([Basename, I, '.png'], File),
 	      Is, Files)
     ).
-      
+
 
 number_of_pages(N) -->
     {
@@ -271,3 +355,13 @@ seq([E|Es]) --> [E], seq(Es).
 
 ... --> [] | [_], ... .
 
+
+dbg_response(Title, Response) :-
+    write("Title: "),
+    writeln(Title),
+    dbg_response(Response).
+dbg_response(Response) :-
+    (  phrase(html(Response), H)
+    -> print_html(H)
+    ;  writeln(Response)
+    ).
